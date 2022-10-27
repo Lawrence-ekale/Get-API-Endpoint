ff04f1258b86b92817fe93b0b1c4f6f4becd82a5
Lawrence Emuria Ekale <85065073+Lawrence-ekale@users.noreply.github.com>
2022-10-27 09:30:31 +0300

    Add files via upload

---
+++ Get-API-Endpoint.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const details={
+	"slackUsername":"Lawrence-Ekale",
+	"backend":true,
+	"age":24,
+	"bio":" I am a forth year student from Moi University currently pursuing my Bachelor's degree in Computer Science. I am cloud passsionate and always eager to learn"
+};
+app.route("/").get((req,res)=>{
+	res.status(200).json(details);
+});
+app.listen(3030);
